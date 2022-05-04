4e819e0fe22d3ce4b47aa89a3360ec5f8c007049
devbyjonah <jonahwagner231@gmail.com>
2022-05-03 22:35:02 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*
+
+	localStorage methods :
+		- key(n) = returns name of nth key 
+		- setItem(data) = adds key and value to storage || updates key if it already exists
+		- getItem(key) = returns value of provided key
+		- removeItem(key) = removes provided key
+		- clear = empties local storage
+
+*/
