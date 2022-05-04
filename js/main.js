5ba08a2e29bbee5604697c4767468d31b913af93
devbyjonah <jonahwagner231@gmail.com>
2022-05-04 00:21:48 -0700

    started implementing Note class

--- js/main.js
+++ js/main.js
@@ -8,3 +8,40 @@
 		- clear = empties local storage
 
 */
+
+class Note{
+	constructor(title, content, created){
+		this._title = title
+		this._content = content
+		this._created = created
+		this._updated = created
+	}
+	get title(){
+		return this._title
+	}
+	set title(newTitle){
+		this._title = newTitle
+	}
+	get content(){
+		return this._content
+	}
+	set content(newContent){
+		this._content = newContent
+	}
+	get created(){
+		return this._created
+	}
+	get updated(){
+		return this._updated
+	}
+	set updated(newDate){
+		this._updated = newDate
+	}
+
+	renderNote(){
+		let title = document.createElement('h1')
+		let content = document.createElement('p')
+	}
+}
+
+let mynote = new Note('title', 'my note', 'date made')
