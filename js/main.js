ce55254e467716c83b0aa93875fbe0d83dd4f660
devbyjonah <jonahwagner231@gmail.com>
2022-05-26 07:37:37 -0700

    started refactoring javascript Note class

--- js/main.js
+++ js/main.js
@@ -9,134 +9,33 @@
 
 */
 
-class Note {
-	constructor(label, title, content, date){
-		this._label = label
-		this._title = title
-		this._content = content
-		this._updated = date
-	}
-	get label(){
-		return this._label
-	}
-	get title(){
-		return this._title
-	}
-	set title(newTitle){
-		this._title = newTitle
-	}
-	get content(){
-		return this._content
-	}
-	set content(newContent){
-		this._content = newContent
-	}
-	get updated(){
-		return this._updated
-	}
-	set updated(newDate){
-		this._updated = newDate
+// The Note object has methods to allow adding a new note, saving a note, deleting a note, and opening a note
+
+// It has 4 properties: key, title, content, and updated
+
+class Note{
+	constructor(title, content, updated){
+		this.key = new Date().toLocaleString()
+		this.title = title
+		this.content = content
+		this.updated = updated
 	}
 
-	renderNote(){
-		document.querySelector('.title').value = this._title
-		document.querySelector('.content').value = this._content
-	}
-
-	renderPreview(){
-		/* create elements */
-		let notePreview = document.createElement('li')
-		let title = document.createElement('h3')
-		let content = document.createElement('p')
-		let updated = document.createElement('span')
-
-		/* assign preview class with this._label */
-		notePreview.id = this._label
-
-		/* attach elements to container */
-		notePreview.appendChild(title)
-		notePreview.appendChild(content)
-		notePreview.appendChild(updated)
-
-		/* assign preview values */
-		title.innerText = this._title
-		content.innerText = this._content
-		updated.innerText = this._updated
-
-		/* append to DOM */
-		document.querySelector('.notesList').appendChild(notePreview)
-	}
-
-	removePreview(){
-		document.querySelector('.' + this._label).remove()
-	}
-}
-
-class NoteStorage{
-	constructor() {
-		this._noteCount = 0
-		this._noteList = {}
-		this._currentNote
-	}
-
-	initialize(){
-		for(let i = 0; i < localStorage.length; i++){
-			let current = JSON.parse(localStorage[(i + 1).toString()])
-			this._noteList[current.label] = current
-			this._noteCount += 1
-			current.renderPreview()
-		}
-	}
-
-	get noteList(){
-		return this._noteList
-	}
-
-	get currentNote(){
-		return this._currentNote
-	}
-
-	set currentNote(note){
-		this._currentNote = note
-	}
-
-	createNote(){
-		this._noteCount++
-		let newNote = new Note(this._noteCount, 'Enter title here . . .', 'Write your thoughts here . . .', Date())
-		this._currentNote = newNote
-		newNote.renderNote()
+	deleteNote(){
+		localStorage.removeItem(this.key)
 	}
 
 	saveNote(){
-		if (!this.currentNote){
-			let newNote = new Note(this._noteCount, document.querySelector('.title').innerText, document.querySelector('.content').innerText, Date())
-			this._currentNote = newNote
-		}
-		this._noteCount += 1
-		this._currentNote.updated = Date()
-		localStorage.setItem(this._noteCount, JSON.stringify(this._currentNote))
+		this.title = document.querySelector('.title').value
+		this.content = document.querySelector('.content').innerText
+
+		localStorage.setItem(this.key, JSON.stringify(this))
 	}
 
-	deleteNote(note){
-		localStorage.removeItem(note.label)
-		note.removePreview()
-		this._noteCount -= 1
+	openNote(){
+		document.querySelector('.title').value = this.title
+		document.querySelector('.content').innerText = this.content
 	}
 }
 
-/* button setup */
-const saveButton = document.querySelector('.saveButton')
-const addButton = document.querySelector('.addButton')
-const preview = document.querySelector('li')
-
-/* create notes object to store and manage all notes */
-let notes = new NoteStorage()
-notes.initialize()
-
-/* event listeners*/
-saveButton.addEventListener('click', () => notes.saveNote())
-addButton.addEventListener('click', () => notes.createNote())
-preview.addEventListener('click', (val) => {
-	notes.noteList[val.id].renderNote()
-	notes.currentNote = notes.noteList[val.id]
-})
+let defaultNote = new Note('', '', new Date().toLocaleString)
